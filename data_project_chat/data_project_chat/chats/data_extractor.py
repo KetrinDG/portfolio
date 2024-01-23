b9536e7ea04d25f4876a765df22505b4bb68080d
KetrinDG <degtyareva.ev1@gmail.com>
2024-01-23 21:36:51 +0200

    up

---
+++ data_project_chat/data_project_chat/chats/data_extractor.py
@@ -0,0 +1,71 @@
+from io import BytesIO
+from os.path import splitext
+from abc import abstractclassmethod, ABC
+
+#import fitz
+import pdfplumber
+from docx2txt import process
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+
+class Reader(ABC):
+
+    @abstractclassmethod
+    def read(self, file: InMemoryUploadedFile) -> str:
+        pass
+
+
+# class PDFReader(Reader):
+
+#     def read(self, file: InMemoryUploadedFile) -> str:
+#         text = ""
+#         in_memory_pdf = BytesIO(file.read())  
+#         document = fitz.open(stream=in_memory_pdf, filetype="pdf")
+#         for page_num in range(document.page_count):
+#             page = document.load_page(page_num)
+#             text += page.get_text("text")
+#         return text
+
+class PDFReader(Reader):
+
+    def read(self, file: InMemoryUploadedFile) -> str:
+        text = ""
+        in_memory_pdf = BytesIO(file.read())
+        with pdfplumber.open(in_memory_pdf) as pdf:
+            for page in pdf.pages:
+                text += page.extract_text()
+        return text
+
+class TXTReader(Reader):
+
+    def read(self, file: InMemoryUploadedFile) -> str:
+        text = file.file.read()
+        return text.decode("utf-8")
+
+
+class DOCXReader(Reader):
+
+    def read(self, file: InMemoryUploadedFile) -> str:
+        text = process(file.file)
+        return text
+    
+
+READERS = {'.pdf': PDFReader(), '.txt': TXTReader(), '.docx': DOCXReader()}
+
+
+def read_file(file: InMemoryUploadedFile) -> str:
+    filename = file.name
+    _, file_extension = splitext(filename)
+
+    try:
+        reader = READERS[file_extension]
+    except KeyError as e:
+        return None
+    
+    return reader.read(file)
+    
+
+def normalize_filename(filename):
+    filename = filename.replace(" ", "_")
+    filename = filename.lower()
+    return filename
